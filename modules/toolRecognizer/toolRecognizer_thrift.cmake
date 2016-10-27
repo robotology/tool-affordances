a228d7c862ab7eb7226ad44211742eec6106b8a7
Tanis Mar <tanis.mar@iit.it>
2016-10-27 16:04:09 +0200

    removed toolRecognizer, moved to objects3Dexplorer

--- modules/toolRecognizer/toolRecognizer_thrift.cmake
+++
@@ -1,5 +0,0 @@
-## This is an automatically-generated file.
-## It could get re-generated if the ALLOW_IDL_GENERATION flag is on
-
-set(headers include/BoundingBox.h;include/toolRecognizer_IDLServer.h)
-set(sources src/BoundingBox.cpp;src/toolRecognizer_IDLServer.cpp)
