4f4994922d48b811264b341cec2ff483ea455d3f
Tanis Mar <tanis.mar@iit.it>
2017-10-18 10:36:25 +0200

    IDL changes

--- modules/graspChecker/include/BoundingBox.h
+++ modules/graspChecker/include/BoundingBox.h
@@ -48,10 +48,10 @@ public:
   }
 
   // read and write structure on a connection
-  bool read(yarp::os::idl::WireReader& reader) YARP_OVERRIDE;
-  bool read(yarp::os::ConnectionReader& connection) YARP_OVERRIDE;
-  bool write(yarp::os::idl::WireWriter& writer) YARP_OVERRIDE;
-  bool write(yarp::os::ConnectionWriter& connection) YARP_OVERRIDE;
+  bool read(yarp::os::idl::WireReader& reader) override;
+  bool read(yarp::os::ConnectionReader& connection) override;
+  bool write(yarp::os::idl::WireWriter& writer) override;
+  bool write(yarp::os::ConnectionWriter& connection) override;
 
 private:
   bool write_tlx(yarp::os::idl::WireWriter& writer);
@@ -171,8 +171,8 @@ public:
     void clean() {
       dirty_flags(false);
     }
-    bool read(yarp::os::ConnectionReader& connection) YARP_OVERRIDE;
-    bool write(yarp::os::ConnectionWriter& connection) YARP_OVERRIDE;
+    bool read(yarp::os::ConnectionReader& connection) override;
+    bool write(yarp::os::ConnectionWriter& connection) override;
   private:
 
     BoundingBox *obj;
